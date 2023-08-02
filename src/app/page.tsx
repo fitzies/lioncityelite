7cf31b328b5cc38465f70ebb68f37a82b839e1ed
Mr Bushido <114846273+fitzies@users.noreply.github.com>
2023-08-02 12:44:10 +0800

    in-progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 
 const Page = ({ params }: { params: { slug: string } }) => {
   return (
-    <div className="w-screen h-screen bg-[#171717]">
+    <div className="w-screen h-screen bg-[#171717] flex justify-center items-center">
       <h1 className="font-bold text-4xl">Website in progress</h1>
     </div>
   );
